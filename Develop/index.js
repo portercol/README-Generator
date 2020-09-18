2c90eb917d2680107bc7d2820848189dafdf4484
Collin Porter <colls.potta@gmail.com>
2020-09-18 17:13:25 -0600

    added array of questions to the questions variable

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,54 @@
-// array of questions for user
-const questions = [
 
-];
+
+var inquirer = require("inquirer");
+// array of questions for user
+const questions = ([
+{
+    type: "input",
+    name: "title",
+    message: "What is the title of your README?"
+},
+{
+    type: "input",
+    name: "description",
+    message: "What is the description of your project?"
+},
+{
+    type: "input",
+    name: "installation",
+    message: "What are the installation instructions?"
+},
+{
+    type: "input",
+    name: "tableofcontents",
+    message: "What are the table of contents?"
+},
+{
+    type: "input",
+    name: "license",
+    message: "What is the licensure of your project?"
+},
+{
+    type: "input",
+    name: "contributing",
+    message: "Do you have any contributors on your project?"
+},
+{
+    type: "input",
+    name: "usage",
+    message: "What is your project used for?"
+},
+{
+    type: "input",
+    name: "tests",
+    message: "What are the tests for your project?"
+},
+{
+    type: "input",
+    name: "questions",
+    message: "Are there any questions for your project?"
+}
+]);
 
 // function to write README file
 function writeToFile(fileName, data) {
