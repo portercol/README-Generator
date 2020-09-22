036477092d8cc38cf9cb69bd1d69465472b02536
Collin Porter <colls.potta@gmail.com>
2020-09-22 16:39:01 -0600

    made function for writeReadme & stored gm in variable

--- Develop/index.js
+++ Develop/index.js
@@ -6,81 +6,83 @@ const fs = require("fs");
 // using inquirer, create an array of questions for user
 
 const questions = inquirer.prompt([
-{
-    type: "input",
-    name: "title",
-    message: "What is the title of your README?"
-},
-{
-    type: "input",
-    name: "description",
-    message: "What is the description of your project?"
-},
-{
-    type: "input",
-    name: "installation",
-    message: "What are the installation instructions?"
-},
-{
-    type: "input",
-    name: "contents",
-    message: "What are the table of contents?"
-},
-{
-    type: "list",
-    name: "licenseList",
-    message: "Which license would you like to use?",
-    choices: [
-        'Apache License 2.0',
-        'BSD 3-Clause (New or Revised) license',
-        'BSD 2-Clause (Simplified or FreeBSD) license',
-        'GNU (General Public License GPL)',
-        'GNU Library or (Lesser General Public License LGPL)',
-        'MIT license',
-        'Mozilla Public License 2.0',
-        'Common Development and Distribution License',
-        'Eclipse Public License version 2.0'
-    ]
-},
-{
-    type: "input",
-    name: "contributing",
-    message: "Do you have any contributors on your project?"
-},
-{
-    type: "input",
-    name: "usage",
-    message: "What is your project used for?"
-},
-{
-    type: "input",
-    name: "tests",
-    message: "What are the tests for your project?"
-},
-{
-    type: "input",
-    name: "questions",
-    message: "Are there any questions for your project?"
-}
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your README?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is the description of your project?"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the installation instructions?"
+    },
+    {
+        type: "input",
+        name: "contents",
+        message: "What are the table of contents?"
+    },
+    {
+        type: "list",
+        name: "licenseList",
+        message: "Which license would you like to use?",
+        choices: [
+            'Apache License 2.0',
+            'BSD 3-Clause (New or Revised) license',
+            'BSD 2-Clause (Simplified or FreeBSD) license',
+            'GNU (General Public License GPL)',
+            'GNU Library or (Lesser General Public License LGPL)',
+            'MIT license',
+            'Mozilla Public License 2.0',
+            'Common Development and Distribution License',
+            'Eclipse Public License version 2.0'
+        ]
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Do you have any contributors on your project?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What is your project used for?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "What are the tests for your project?"
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "Are there any questions for your project?"
+    }
 
 ])
 
-// promise is here
+    // promise is here
 
-.then(function(response){
-    console.log(response)
-});
-
+    .then(function (response) {
+        var temp = util(response)
+        writeReadme(temp)
+    });
+    
 // function to write README file
 
-fs.writeFile('README.md', 'README.md Generator', (err) => {
-// if there are any errors in writing to the file, return below
-	if (err) {
-		console.error(err)
-		return
-	}
-});
-
+function writeReadme(temp) {
+    fs.writeFile('README.md', temp, (err) => {
+        // if there are any errors in writing to the file, return below
+        if (err) {
+            console.error(err)
+            return
+        }
+    });
+}
 // function to initialize program
 
 function init() {
