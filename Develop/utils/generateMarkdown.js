65263d9b82bb77cdd0e0b32817804fb283451e92
Collin Porter <colls.potta@gmail.com>
2020-09-21 18:56:54 -0600

    entered text into generateMarkdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,30 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
     return `# ${data.title}
-  
+
+    ## Description
+    ${data.description}
+
+    ## Installation
+    ${data.installation}
+
+    ## Table of Contents
+    ${data.contents}
+
+    ## License
+    ${data.LicenseList}
+
+    ## Contributors
+    ${data.contributing}
+
+    ## Usage
+    ${data.usage}
+
+    ## Tests
+    ${data.tests}
+
+    ## Questions
+    ${data.questions}
   `;
   }
   
